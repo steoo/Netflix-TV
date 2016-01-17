2acdd4f76d5e831eb4624e615a8732f6c9a458a8
Steoo <esseiproject@gmail.com>
2016-01-17 02:49:18 +0100

    improvements

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,4 @@
+var div = '<div class="lolomoRow lolomoRow_title_card"><h2 class="rowHeader"><a href="/browse/genre/1365" class="rowTitle">Channel 1</a></h2></div>';
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -7,7 +8,15 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-
+		appendChannels();
 	}
 	}, 10);
 });
+
+
+function appendChannels(){
+	console.log(jQuery);
+	$after = (".billboard-row");
+	$div = $(div);
+	$(div).insertAfter($after);
+}
